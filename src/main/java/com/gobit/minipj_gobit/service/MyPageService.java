671aef86d7bce51670b22e9be9b7c8ea005697f8
Eunhee-nim <126763448+Eunhee-nim@users.noreply.github.com>
2023-06-20 15:29:21 +0900

    MypageTest

---
+++ src/main/java/com/gobit/minipj_gobit/service/MyPageService.java
@@ -0,0 +1,11 @@
+package com.gobit.minipj_gobit.service;
+
+import com.gobit.minipj_gobit.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyPageService {
+    private UserRepository userRepository;
+    
+
+}
