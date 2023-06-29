53d5a961f9f420af7387ef59ee6efab3461e9a25
YunHyebin <qpqpqp17820@gmail.com>
2023-06-29 16:15:24 +0900

    초기화

--- src/main/java/com/gobit/minipj_gobit/repository/AppMeetRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.gobit.minipj_gobit.repository;
-//
-//import com.gobit.minipj_gobit.entity.AppMeeting;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AppMeetRepository extends JpaRepository<AppMeeting, Integer> {
-//}
