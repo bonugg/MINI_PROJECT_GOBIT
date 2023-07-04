b5065f51509b8269e9569c48e4c5d850d9bf238e
redwolf2023 <wjdtn8046@naver.com>
2023-07-04 18:56:05 +0900

    지해님 자리로 ㄱ가주세요요

--- src/main/java/com/gobit/minipj_gobit/noticeDept/repository/nBoardRepository.java
+++ src/main/java/com/gobit/minipj_gobit/noticeDept/repository/nBoardRepository.java
@@ -2,6 +2,7 @@ package com.gobit.minipj_gobit.noticeDept.repository;
 
 import com.gobit.minipj_gobit.noticeDept.entity.nBoard;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,8 @@ import org.springframework.stereotype.Repository;
 public interface nBoardRepository extends JpaRepository<nBoard, Long> {
 
     Page<nBoard> findAll(Pageable pageable);
+
+    Page<nBoard> findByContentContainingOrTitleContaining(String searchKeyword, String keyword,Pageable pageable);
+
 }
+
