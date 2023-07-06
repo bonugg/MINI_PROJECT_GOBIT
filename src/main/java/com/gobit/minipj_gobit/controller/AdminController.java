f29212d25e7ee792b969f9f84e43aa8729b16900
dxnx-hh <dxxg.dxnx@gmail.com>
2023-07-06 20:23:26 +0900

    3

--- src/main/java/com/gobit/minipj_gobit/controller/AdminController.java
+++ src/main/java/com/gobit/minipj_gobit/controller/AdminController.java
@@ -33,7 +33,7 @@ public class AdminController {
         mv.setViewName("admin/adminMemberPage");
         return mv;
     }
-
+//dasdasdasda
     @PostMapping("/changePosition")
     public String changePosition(@RequestParam("usernum") long usernum,
                                  @RequestParam("position") String position) {
